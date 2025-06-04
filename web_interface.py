ed1065b5340d2346deda355036fdf625397329f1
O_Stary_ld <gardungadun@gmail.com>
2025-06-04 18:14:15 +0300

    Remove authentication and update styles

--- web_interface.py
+++ web_interface.py
@@ -1,66 +1,25 @@
-from datetime import timedelta
 import random
 import time
 import eventlet
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask_jwt_extended import (
-    JWTManager,
-    create_access_token,
-    jwt_required,
-    decode_token,
-)
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 from data_storage import DataStorage
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'change-this-secret'
-app.config['JWT_SECRET_KEY'] = 'change-this-jwt-secret'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
-
-jwt = JWTManager(app)
 # use eventlet for async workers
 socketio = SocketIO(app, cors_allowed_origins='*', async_mode='eventlet')
 
 storage = DataStorage()
 
-
-@jwt.unauthorized_loader
-def unauthorized_callback(reason):
-    return redirect(url_for('login_page'))
-
-
-@app.post('/login')
-def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-    if username and password:
-        token = create_access_token(identity=username)
-        return jsonify(access_token=token)
-    return jsonify({'msg': 'Bad credentials'}), 401
-
-
-@app.get('/login')
-def login_page():
-    return render_template('login.html')
-
-
 @app.get('/')
-@jwt_required()
 def index():
     return render_template('index.html')
 
 
 @socketio.on('connect')
 def handle_connect():
-    token = request.args.get('token')
-    if not token:
-        return False  # disconnect
-    try:
-        decode_token(token)
-    except Exception:
-        return False
 
     def send_coords():
         while True:
